56c380770d0c09a80d2ed164f9f07348f45d1689
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-07-05 16:17:20 -0300

    commit_ creatnewaller

---
+++ WalletService.js
@@ -0,0 +1,14 @@
+const { ethers } = require("ethers"); 
+
+const provider = new ethers.JsonRpcProvider(process.env.BLOCKCHAIN_NODE);
+
+let myWallet = null;
+
+function createWallet(){
+    myWallet = ethers.Wallet.createRandom(provider);
+    return myWallet;
+}
+
+module.exports = {
+    createWallet
+}
--- index.js
+++ index.js
@@ -1,22 +1,36 @@
+require("dotenv").config();
+
+const WalletService = require("./WalletService");
+const SYMBOL = process.env.SYMBOL;
+
+const { config } = require("dotenv");
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+let myAddress = null;
+
 function menu() {
     setTimeout(() => {
     console.clear();
 
+    if(myAddress){
+        console.log(`You are logged as ${myAddress}`);
+    }else{
+        console.log(`You aren't logged!`);
+    }
+
         console.log("1 - Create Wallet");
         console.log("2 - Recover Wallet");
         console.log("3 - Balance");
-        console.log("4 - Send");
+        console.log("4 - Send " + SYMBOL);
         console.log("5 - Seaech Tx");
 
         rl.question("Choose your option: ", (answer) => {
             switch(answer){
-                case "1": break;
+                case "1": createWallet(); break;
                 case "2": break;
                 case "3": break;
                 case "4": break;
@@ -28,6 +42,21 @@ function menu() {
             }
         })
     },2000)
+}
+
+function preMenu(){
+    rl.question("Press any key to continue...", () => {
+        menu();
+    })
+}
+
+function createWallet(){
+    const myWallet = WalletService.createWallet();
+    myAddress = myWallet.address;
+
+    console.log(`Your new Wallet:`);
+    console.log(myAddress);
+    preMenu();
 }
 
 menu();
